be4dda0c95936bc6535dd432e56f2b3022a85d20
AlfonsoMiniPeta2 <alfonsojahred@gmail.com>
2026-09-17 16:45:01 +0800

    added AlfonsoMiniPeta2

---
+++ app/src/test/java/com/example/bippsqr2/AlfonsoMiniPeta2.java
@@ -0,0 +1,31 @@
+package com.example.bippsqr2;
+
+import org.junit.Test;
+
+public class AlfonsoMiniPeta2 {package Prop;
+    trackpack.quarter2;ackage Prop;
+    trackpack.quarter2;
+
+
+import org.junit.Test;
+
+
+    public class Package {
+        @Test
+        public void printMyProfile () {
+            // --- 1. THE INPUT (Storing your personal details in variables) ---
+            String myName = "Jahred Alfonso";
+            String petName = "Uno";
+            String favFood = "FriedChicken";
+            int myAge = 17;
+
+
+            // --- 2. THE OUTPUT (Printing to the Console)
+            System.out.println("--- MY DIGITAL PROFILE ---");
+            System.out.println("Hello, my name is " + myName + " and I am " + myAge + "years old.");
+            System.out.println("I have a wonderful pet named " + petName + ".");
+            System.out.println("If I could, I would eat " + favFood + " every single day ");
+
+
+
+}
