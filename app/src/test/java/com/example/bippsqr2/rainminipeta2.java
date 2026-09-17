2d2f7da400ffc30a6a21504c535b0ce573571ca4
mendozaminipeta2 <dyremsmendoza@gmail.com>
2026-09-17 03:34:24 -0700

    mendozaminipeta2

---
+++ app/src/test/java/com/example/bippsqr2/rainminipeta2.java
@@ -0,0 +1,33 @@
+package com.example.bippsqr2;
+
+public class rainminipeta2 import org.junit.Test;
+
+    public class rainminiPeta2 {package Prop;
+        trackpack.quarter2;ackage Prop;
+        trackpack.quarter2;
+        public class mendozaMiniPeta2{}
+
+
+import org.junit.Test;
+
+
+        public class Package {
+            @test
+            public void printMyProfile () {
+                // --- 1. THE INPUT (Storing your personal details in variables) ---
+                String myName = "Rain Mendoza";
+                String petName = "Sky";
+                String favFood = "Manok";
+                int myAge = 17;
+
+
+                // --- 2. THE OUTPUT (Printing to the Console)
+                System.out.println("--- MY DIGITAL PROFILE ---");
+                System.out.println("Hello, my name is " + myName + " and I am " + myAge + "years old.");
+                System.out.println("I have a wonderful pet named " + petName + ".");
+                System.out.println("If I could, I would eat " + favFood + " every single day ");
+
+
+
+            }
+
