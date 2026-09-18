5a6f4c27d27869ebfb536852008f80d54611c1e1
SHS PC <ethanbradley909@gmail.com>
2026-09-18 08:48:41 +0800

    amen

---
+++ app/src/test/java/com/example/bippsqr2/Mcminipeta2.java
@@ -0,0 +1,19 @@
+package com.example.bippsqr2;
+import org.junit.Test;
+public class Mcminipeta2 {
+
+    @Test
+    public void printMyProfile() {
+        String myName = "Mc Farell";
+        String petName = "Moon";
+        String favFood = "burger steak";
+        int myAge = 18;
+
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + "years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day ");
+
+    }
+}
